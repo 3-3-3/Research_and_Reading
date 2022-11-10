27e5f4e52c628565710781aac57788e722f37e7b
3-3-3 <l_keeley@coloradocollege.edu>
2022-11-09 23:57:09 -0700

    Fundamentals of plasma physics p.1.8

--- Fundamentals_of_Plasma_Physics/Basic_Concepts/p_1_8.py
+++ Fundamentals_of_Plasma_Physics/Basic_Concepts/p_1_8.py
@@ -2,44 +2,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.constants as pc
 
-def E(r_1, r_2, q_1):
-    #E from relative positions of particles, charge
-    r = np.sqrt(np.dot(r_1, r_2))
-    r_hat = (r_2 - r_1) / r #unit vector in the direction of r_2 from r_1
-    return q_1 / (4 * np.pi * pc.epsilon_0 * r ** 2) * r_hat
+class Collision:
+    def __init__(self, t_f, r_1, r_2, v_1, v_2, q_1=-pc.e, q_2=pc.e, m_1=pc.m_e, m_2=pc.m_p):
+        self.step = 0
+        self.init = [[r_1, v_1], [r_2, v_2]]
+        self.t_f = t_f
 
-def Sigma(q,m,E):
-    #Normalized electric field vector
-    return q * E / m
+        self.r_1 = r_1; self.r_2 = r_2
+        self.v_1 = v_1; self.v_2 = v_2
 
-def v_next(dt, v_old, Sigma):
-    return v_old + Sigma * dt
+        self.q_1 = q_1; self.q_2 = q_2
+        self.m_1 = m_1; self.m_2 = m_2
 
-def simulation(t_f, r_1, r_2, v_1, v_2, q_1, q_2, m_1, m_2):
-    m = (m_1 * m_2) / (m_1 + m_2) #reduced mass of the system
-    v_inf = v_2 - v_1 #characteristic speed; speeds are in +/- z
-    b_pi_2 =  (q_1 * q_2) / (4 * np.pi * pc.epsilon_0 * m * v_inf) #characteristic length; 90 degree impact paramter, as found in 1.1
-    dt = b_pi_2 / v_inf #characteristic time, time it takes particle to move characteristic length at characteristic speed
+        self.m = (m_1 * m_2) / (m_1 + m_2)
 
-    steps = int(t_f / dt)
-    t = np.linspace(0, t_f, steps) #times to calculate next position, velocity; multiples of characteristic time
-    p_1 = (np.array(r_1),np.array(v_1)) #particle 1 info; position, velocity
-    p_2 = (np.array(r_2),np.array(v_2)) #particle 2 info; position, velocity
+        self.v_inf = np.sqrt(np.abs(np.dot(v_2 - v_1, v_2 - v_1)))
+        self.b_pi_2 =  np.abs((q_1 * q_2) / (4 * np.pi * pc.epsilon_0 * self.m * self.v_inf))
+        self.dt = self.b_pi_2 / (10 * self.v_inf)
 
-    for dt in t:
-        r_1_old = p_1[0][-1]; v_1_old = p_1[1][-1]
-        r_2_old = p_2[0][-1]; v_2_old = p_2[1][-1]
+        t = np.array([])
+        self.r_1_hist = np.empty([500,3]); self.r_2_hist = np.empty([500,3])
+        self.v_1_hist = np.empty([500,3]); self.v_2_hist = np.empty([500,3])
 
-        E_1_2 = E(r_2_old, r_1_old, q_2)
-        E_2_1 = E(r_1_old, r_2_old, q_1)
+    def sigma_1(self):
+        #Normalized E-field at q_1 due to q_2
+        R = self.r_1 - self.r_2
+        R_mag = np.sqrt(np.abs(np.dot(R, R)))
+        return (self.q_1 / self.m_1) * self.q_2 * R / (4 * np.pi * pc.epsilon_0 * R_mag ** 3)
 
-        v_1_new = v_next(dt, v_2_old, Sigma(q_1, m_1, E_1_2))
-        v_2_new = v_next(dt, v_1_old, Sigma(q_2, m_2, E_2_1))
+    def sigma_2(self):
+        #Flippoty Flop
+        R = self.r_2 - self.r_1
+        R_mag = np.sqrt(np.abs(np.dot(R, R)))
+        return (self.q_2 / self.m_2) * self.q_1 * R / (4 * np.pi * pc.epsilon_0 * R_mag ** 3)
 
-        r_1_new = r_1_old + v_1_new * dt
-        r_2_new = r_2_old + v_2_new * dt
 
-        p_1[0] = np.append(p_1[0], r_1_new, axis=0); p_1[1] = np.append(p_1[1], v_1_new, axis=0)
-        p_2[0] = np.append(p_2[0], r_2_new, axis=0); p_2[1] = np.append(p_2[1], v_2_new, axis=0)
+    def next(self):
+        self.v_1 = self.v_1 + self.sigma_1() * self.dt
+        self.v_2 = self.v_2 + self.sigma_2() * self.dt
 
-    return (p_1, p_2)
+        self.r_1 = self.r_1 + self.v_1 * self.dt
+        self.r_2 = self.r_2 + self.v_2 * self.dt
+
+        self.r_1_hist[self.step] = self.r_1
+        self.r_2_hist[self.step] = self.r_2
+        self.v_1_hist[self.step] = self.v_1
+        self.v_2_hist[self.step] = self.v_2
+
+        self.step += 1
+
+    def plot(self):
+        plt.plot(self.r_1_hist[:self.step-1,1],self.r_1_hist[:self.step-1,2])
+        plt.plot(self.r_2_hist[:self.step-1,1],self.r_2_hist[:self.step-1,2], color = 'red')
+        plt.show()
